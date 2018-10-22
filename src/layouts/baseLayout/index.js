7128076598aaacd2f6899b518742d4999efa7092
eric ma <924402086@qq.com>
2018-10-22 16:55:35 +0800

    添加了面包屑组件

--- src/layouts/baseLayout/index.js
+++ src/layouts/baseLayout/index.js
@@ -1,11 +1,12 @@
-import styles from './styles.less';
 import withRouter from 'umi/withRouter';
 import Link from 'umi/link';
+import Breadcrumbs from '../../components/Breadcrumbs';
 import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 export default withRouter(
   ({ location, children }) =>
     <div>
+      <Breadcrumbs/>
       <div>
         <Link to="/">home</Link>
       </div>
